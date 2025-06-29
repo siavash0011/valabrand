b15a0f4c9d2f0e24ea75a4b88c50a5a982d2f062
siavash <siavashnajafi01@gmail.com>
2025-06-29 21:23:38 +0400

    path fiexed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages deployment config
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
+export default defineConfig(({ command, mode }) => {
+  // Vite injects `MODE` and `DEV`/`PROD` here
+  const dev = mode === 'development'
+
+  return {
+    base: dev ? '/' : '/valabrand/',
+    plugins: [react()],
+    server: {
+      port: 3000,
+      strictPort: true,
+    },
+  }
 })
